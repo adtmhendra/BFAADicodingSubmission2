ecf5226cc307b83c47ca286249b688c3a596ba32
Hendra Pratik Aditama <hendra.pratik09@gmail.com>
2020-08-19 16:21:04 +0700

    Getting data with retrofit

--- Github User Submission 2/app/src/main/java/com/hendra/githubuser/viewmodel/SearchViewModel.kt
+++ Github User Submission 2/app/src/main/java/com/hendra/githubuser/viewmodel/SearchViewModel.kt
@@ -1,26 +1,48 @@
 package com.hendra.githubuser.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.hendra.githubuser.MainActivity
 import com.hendra.githubuser.model.ItemsItem
+import com.hendra.githubuser.model.ResponseUser
 import com.hendra.githubuser.network.ApiConfig
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
 class SearchViewModel : ViewModel() {
 
-    val listUsers = MutableLiveData<ArrayList<ItemsItem>>()
+    val listUsers = MutableLiveData<ItemsItem>()
 
     fun setUser(user: String) {
         val client = ApiConfig.getApiService().getUsers(user)
 
-        client.enqueue(object : retrofit2.Callback<ItemsItem> {
+        client.enqueue(object : Callback<ResponseUser> {
 
-            override fun onResponse(call: Call<ItemsItem>, response: Response<ItemsItem>) {
+            override fun onResponse(call: Call<ResponseUser>, response: Response<ResponseUser>) {
+                val dataArray = response.body()?.data as List<ItemsItem?>
+                Log.d(MainActivity.TAG, dataArray.toString())
+                try {
+                    for (data in dataArray) {
+                        listUsers.postValue(data)
+                    }
+                } catch (e: Exception) {
+                    Log.d(MainActivity.TAG, "Gagal @onResponse...")
+                }
             }
 
-            override fun onFailure(call: Call<ItemsItem>, t: Throwable) {
+            override fun onFailure(call: Call<ResponseUser>, t: Throwable) {
+                Log.d(MainActivity.TAG, t.message.toString())
             }
+
         })
     }
+
+    fun getUser(): LiveData<ItemsItem> {
+        return listUsers
+    }
+
 }
